01577c8292c66b284fc4c5dc91ac7804859503a5
int3hh <>
2025-03-26 21:49:08 +0200

    gnome 47, fixes

--- 202020ruletimer@202020ruletimer.com/extension.js
+++ 202020ruletimer@202020ruletimer.com/extension.js
@@ -1,50 +1,44 @@
 import GObject from 'gi://GObject';
 import St from 'gi://St';
 import GLib from 'gi://GLib';
+import Gio from 'gi://Gio';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
-
+import * as MessageTray from 'resource:///org/gnome/shell/ui/messageTray.js';
 import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
-
-
-let notificationTimeoutId;
-
-function showNotification() {
-    Main.notify('Time to rest your eyes', 'Look 20 feet away for 20 seconds');
-    notificationTimeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1200, () => {
-        showNotification();
-        notificationTimeoutId = null;
-        return GLib.SOURCE_REMOVE;
-    });
-}
-
 
 const Indicator = GObject.registerClass(
 class Indicator extends PanelMenu.Button {
     _init() {
         super._init(0.0, _('Eye Care Reminder'));
-
         this.add_child(new St.Icon({
-            icon_name: 'face-smile-symbolic',
+            icon_name: 'face-angry-symbolic',
             style_class: 'system-status-icon',
         }));
-
-        this._timeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1200000, () => {
-            showNotification();
+        this._timeoutId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1200, () => {
+            this._showNotification();
             return GLib.SOURCE_CONTINUE;
         });
-
         let item = new PopupMenu.PopupMenuItem(_('Show Notification'));
         item.connect('activate', () => {
-            showNotification();
+            this._showNotification();
         });
         this.menu.addMenuItem(item);
     }
-
+    
+    _showNotification() {
+        Main.notify('Rest your eyes', 'The code can wait!', { 
+            gicon: new Gio.ThemedIcon({ name: 'face-angry-symbolic' }), 
+            bannerMarkup: true,
+            resident: true
+        });
+    }
+    
     destroy() {
         if (this._timeoutId) {
             GLib.Source.remove(this._timeoutId);
+            this._timeoutId = null;
         }
         super.destroy();
     }
@@ -53,17 +47,13 @@ class Indicator extends PanelMenu.Button {
 export default class EyeCareReminderExtension extends Extension {
     enable() {
         this._indicator = new Indicator();
-        Main.panel.addToStatusArea(this.uuid, this._indicator);
+        Main.panel.addToStatusArea(this.uuid, this._indicator, 1, 'left');
     }
-
+    
     disable() {
         if (this._indicator) {
             this._indicator.destroy();
             this._indicator = null;
-        }
-        if (notificationTimeoutId) {
-            GLib.Source.remove(notificationTimeoutId);
-            notificationTimeoutId = null;
         }
     }
 }
